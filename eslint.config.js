86f2d529153f2ff83d8b40ef967abf9536653d77
Arman Lidder <arman.lidder@polymtl.ca>
2026-05-26 22:00:00 -0400

    Ignore .vite cache in ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'Pathfinding-Visualizer-master/**']),
+  globalIgnores(['dist', 'Pathfinding-Visualizer-master/**', '.vite/**']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
